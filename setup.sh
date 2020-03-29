2ab0de203e2abb4f8c4c806b707150530fa386ca
Yishai Jaffe <yishai1999@gmail.com>
2020-03-29 18:39:50 +0300

    added setup script

---
+++ setup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+SETUP_DIR=$(dirname "$0")
+
+# Init
+Init() {
+	sudo apt -y install curl
+	sudo apt -y install software-properties-common
+}
+
+GetTmux() {
+	sudo apt -y install tmux
+}
+
+# Get git
+GetGit() {
+        sudo apt -y install git
+}
+
+# Get zsh + oh-my-zsh
+GetZsh() {
+	sudo apt -y install zsh
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	cp $SETUP_DIR/.zshrc ~/.zshrc
+}
+
+# Get gstreamer
+GetGstreamer() {
+	sudo apt -y install libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
+}
+
+# Get cmake
+GetCMake() {
+	sudo apt -y install cmake
+}
+
+# Get vim
+GetVim() {
+	sudo apt -y install vim
+}
+
+# Get gdb
+GetGdb() {
+	sudo apt -y install gdb
+}
+
+Init
+GetTmux
+GetGit
+GetZsh
+GetGstreamer
+GetCMake
+GetVim
+GetGdb
